1107bd1dc4d15357f76da355abd27da0ce438006
Kathy Dieser <katdiese@gmail.com>
2016-04-17 13:59:07 -0600

    added users to posts

--- queries.js
+++ queries.js
@@ -11,7 +11,7 @@ function Comments() {
 
 module.exports = {
   getPosts: function() {
-    return Posts().select();
+    return Posts().select().innerJoin('users', 'users.user_id', 'posts.user_id');
   },
   getPostCommentsAndUser: function(id) {
     return Posts().where('posts.post_id',id).innerJoin('comments', 'comments.post_id', 'posts.post_id').innerJoin('users', 'users.user_id', 'posts.user_id');
