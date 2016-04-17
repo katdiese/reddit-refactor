3032ce85a5441f67287d65aaba4aa80a73d9fb4d
Kathy Dieser <katdiese@gmail.com>
2016-04-17 13:40:26 -0600

    queries to d

--- queries.js
+++ queries.js
@@ -15,5 +15,8 @@ module.exports = {
   },
   getPostCommentsAndUser: function(id) {
     return Posts().where('posts.post_id',id).innerJoin('comments', 'comments.post_id', 'posts.post_id').innerJoin('users', 'users.user_id', 'posts.user_id');
+  },
+  addPost: function(post) {
+    return Posts().insert(post);
   }
 }
