d2479ddc58a02a8fb726beeb7055dfa3530a5c42
Kathy Dieser <katdiese@gmail.com>
2016-04-17 12:27:59 -0600

    included db connection, created separate api queries, wrote api routes for all posts and one post

---
+++ queries.js
@@ -0,0 +1,19 @@
+var knex = require('./db/knex');
+function Posts() {
+  return knex('posts');
+}
+function Users() {
+  return knex('users');
+}
+function Comments() {
+  return knex('comments');
+}
+
+module.exports = {
+  getPosts: function() {
+    return Posts().select();
+  },
+  getPostCommentsAndUser: function(id) {
+    return Posts().where('posts.post_id',id).innerJoin('comments', 'comments.post_id', 'posts.post_id').innerJoin('users', 'users.user_id', 'posts.user_id');
+  }
+}
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var queries = require('../../../queries');
 
 
-//GET all information for posts
+
+//GET all posts
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  queries.getPosts()
+  .then(function(data) {
+    res.status(200)
+    .json({
+      data: data,
+      message: 'hi you got here'
+    })
+  })
+});
+
+//GET all comments for one post
+router.get('/posts/:id', function(req, res, next) {
+  queries.getPostCommentsAndUser(req.params.id)
+  .then(function(data) {
+    res.status(200)
+    .json({
+      data: data,
+      message: 'woohoo'
+    })
+  })
 });
 
 //POST new post
+
 
 //POST new comment
 
